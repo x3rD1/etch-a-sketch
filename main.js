722c28dd2d953ef516740e589d3a7416ec3470af
Rexx Alfaro <rexxalfaro+github@gmail.com>
2023-04-07 18:58:45 -0700

    Add hover effect

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const container = document.querySelector('.container');
 
-//create 16x16 divs
+//create 16x16 divs with hover effects
 for (let i = 0; i < 256; i++){
-    const block = document.createElement('div');
+   const block = document.createElement('div');
     block.classList.add('block');
     container.appendChild(block);
+    block.addEventListener('mouseover', () => {
+        block.style.backgroundColor = 'black';
+    });
 }
 
